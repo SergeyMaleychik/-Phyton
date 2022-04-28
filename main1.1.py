d7fc0e51102f72e8624fed0a2c35191e95dfe77b
sergeymaleychik <Snmail1984@mail.ru>
2022-04-28 22:57:41 +0500

    дз 1

---
+++ main1.1.py
@@ -0,0 +1,15 @@
+a = 5
+b = 6
+c = 8
+w = int(input('введите число: '))
+q = [a, b, c, w]
+q.sort()
+print(q)
+
+a = 'Антон'
+b = 'Петя'
+c = 'Вася'
+w = input('введите ваше имя: ')
+q = [a, b, c, w]
+q.sort()
+print(q)
